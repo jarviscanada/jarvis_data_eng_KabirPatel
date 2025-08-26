27d62a606f3208bf96613a627f9d2289549c7d0f
patelkabir29 <patelkabir29@gmail.com>
2025-08-26 16:27:43 +0000

    Updated host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -18,8 +18,8 @@ hostname=$(hostname -f)
 cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out" | grep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk -F: '{print $2}' | xargs)
-cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz:" | awk -F: '{print $2}' | xargs | awk '{printf "%d\n",$1}')
-l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk -F: '{print $2}' | egrep -o '[0-9]+' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | grep "Model name:" | grep -oP '@ \K[0-9.]+' | awk '{printf "%d\n",$1*1000}')
+l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache" | awk -F: '{print $2}' | egrep -o '[0-9]+' | head -1 | xargs)
 total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
 timestamp=$(date '+%Y-%m-%d %H:%M:%S') # current timestamp in `2019-11-26 14:40:19` format; use `date` cmd
 
